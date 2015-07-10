3f47a317b35acc27ed78c782f546f7084f5ddaf0
Marcus Bergo <marcusbergo@gmail.com>
2015-07-10 16:32:42 -0300

    CRUD OK

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,20 @@ class ArticlesController < ApplicationController
 		else
 			render 'new'
 		end
+	end
 
-		redirect_to @article
+	def update
+		@article = Article.find(params[:id])
+ 
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def show
@@ -21,6 +33,13 @@ class ArticlesController < ApplicationController
 
 	def index
 		@articles = Article.all
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	private
